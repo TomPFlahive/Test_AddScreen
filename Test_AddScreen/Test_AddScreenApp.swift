553e0225651da51a78ec833b72a962251135d7aa
Tom Flahive <TomPFlahive@gmail.com>
2023-03-28 07:47:09 -0700

    Initial Commit

---
+++ Test_AddScreen/Test_AddScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AddScreenApp.swift
+//  Test_AddScreen
+//
+//  Created by Tom Flahive on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_AddScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
